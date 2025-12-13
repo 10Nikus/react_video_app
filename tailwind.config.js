f373c4ee435def859c58a2af0c6eac62a4c44582
10Nikus <kijasnikodem@gmail.com>
2025-12-13 11:18:03 +0100

    extend Tailwind theme with custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        spaceIndigo: "#2B2D42",
+        coolGrey: "#8D99AE",
+      },
+    },
   },
   plugins: [],
 };
